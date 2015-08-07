25dc3c9d58eab01817597061f35982f2c402fd3c
prashak <prashak@pras-imac.attlocal.net>
2015-08-07 09:08:42 -0400

    i/o addition

--- example/src/example/Exclass.java
+++ example/src/example/Exclass.java
@@ -1,12 +1,18 @@
 package example;
-
+import java.util.Scanner;
 public class Exclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int i=10;
-for(i=0;i<=10;i=i+2)
-	System.out.println("the number is" +i);
+
+System.out.println("Enter the numbers to be added");
+ Scanner sc=new Scanner(System.in);
+ int i=sc.nextInt();
+ int j=sc.nextInt();
+ int x;
+ x=i+j;
+ System.out.println("The answer is " + x);
+
 
 	
 	}
