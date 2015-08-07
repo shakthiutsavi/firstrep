b89437e25baa972c377c6842faf18bdacf6d421d
prashak <prashak@pras-imac.attlocal.net>
2015-08-07 08:36:15 -0400

    first prog

---
+++ example/src/example/Exclass.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Exclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+for(i=0;i<=10;i=i+2)
+	System.out.println("the number is" +i);
+
+	
+	}
+
+}
